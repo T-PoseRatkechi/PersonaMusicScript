914cf73a1d24e14200e930435e76839165c85d1b
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-19 20:20:39 -0500

    Added function visitor.

---
+++ PersonaMusicScript.Library/Parser/FunctionVisitor.cs
@@ -0,0 +1,27 @@
+﻿using Antlr4.Runtime.Misc;
+using PersonaMusicScript.Library.Models;
+
+namespace PersonaMusicScript.Library.Parser;
+
+internal class FunctionVisitor : SourceBaseVisitor<Function>
+{
+    private readonly ExpressionVisitor expressionVisitor;
+
+    public FunctionVisitor(ExpressionVisitor expressionVisitor)
+    {
+        this.expressionVisitor = expressionVisitor;
+    }
+
+    public override Function VisitFunction([NotNull] SourceParser.FunctionContext context)
+    {
+        var name = context.ID().GetText();
+        var args = new List<object>();
+        foreach (var argExp in context.exprList().expression())
+        {
+            var argValue = this.expressionVisitor.Visit(argExp);
+            args.Add(argValue);
+        }
+
+        return new(name, args.ToArray());
+    }
+}
