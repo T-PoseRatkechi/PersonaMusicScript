8d1750433a8b66ba7e80ce7da3b5bf62d26771ac
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-19 20:22:44 -0500

    Added command visitor.

---
+++ PersonaMusicScript.Library/Parser/CommandVisitor.cs
@@ -0,0 +1,21 @@
+﻿using Antlr4.Runtime.Misc;
+using PersonaMusicScript.Library.Models;
+
+namespace PersonaMusicScript.Library.Parser;
+
+internal class CommandVisitor : SourceBaseVisitor<Command>
+{
+    private readonly ExpressionVisitor expressionVisitor;
+
+    public CommandVisitor(ExpressionVisitor expressionVisitor)
+    {
+        this.expressionVisitor = expressionVisitor;
+    }
+
+    public override Command VisitCommand([NotNull] SourceParser.CommandContext context)
+    {
+        var name = context.ID().GetText();
+        var value = this.expressionVisitor.Visit(context.expression());
+        return new(name, value);
+    }
+}
