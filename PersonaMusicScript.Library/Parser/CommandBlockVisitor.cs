94f3740f4c36327635f94b3e725c8bff2a5afca8
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-19 20:23:11 -0500

    Added command block visitor.

---
+++ PersonaMusicScript.Library/Parser/CommandBlockVisitor.cs
@@ -0,0 +1,31 @@
+﻿using Antlr4.Runtime.Misc;
+using PersonaMusicScript.Library.Models;
+
+namespace PersonaMusicScript.Library.Parser;
+
+internal class CommandBlockVisitor : SourceBaseVisitor<CommandBlock>
+{
+    private readonly ExpressionVisitor expressionVisitor;
+    private readonly CommandVisitor commandVisitor;
+
+    public CommandBlockVisitor(ExpressionVisitor expressionVisitor)
+    {
+        this.expressionVisitor = expressionVisitor;
+        this.commandVisitor = new(expressionVisitor);
+    }
+
+    public override CommandBlock VisitCommandBlock([NotNull] SourceParser.CommandBlockContext context)
+    {
+        var typeString = context.ID().GetText();
+        var type = Enum.Parse<CommandBlockType>(typeString, true);
+        var arg = this.expressionVisitor.Visit(context.expression());
+        var commands = new List<Command>();
+        foreach (var commandCtx in context.command())
+        {
+            var command = this.commandVisitor.Visit(commandCtx);
+            commands.Add(command);
+        }
+
+        return new(type, arg, commands.ToArray());
+    }
+}
