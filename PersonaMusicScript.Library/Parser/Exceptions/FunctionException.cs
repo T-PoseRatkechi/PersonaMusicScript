ad4d95f4106618bc81b821a717fd3d8a89dcc762
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-20 11:31:57 -0500

    Added function exception.

---
+++ PersonaMusicScript.Library/Parser/Exceptions/FunctionException.cs
@@ -0,0 +1,11 @@
+﻿using Antlr4.Runtime;
+
+namespace PersonaMusicScript.Library.Parser.Exceptions;
+
+internal class FunctionException : ParsingException
+{
+    public FunctionException(string name, int expected, int actual, ParserRuleContext context)
+        : base($"Function \"{name}\" expects {expected} arguments. Actual: {actual}", context)
+    {
+    }
+}
