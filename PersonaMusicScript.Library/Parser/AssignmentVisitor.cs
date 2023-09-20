681144081db3e50fecf6005a3238c07f384be3df
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-19 19:07:33 -0500

    Added assignment visitor.

---
+++ PersonaMusicScript.Library/Parser/AssignmentVisitor.cs
@@ -0,0 +1,28 @@
+﻿using Antlr4.Runtime.Misc;
+using PersonaMusicScript.Library.Parser.Exceptions;
+
+namespace PersonaMusicScript.Library.Parser;
+
+internal class AssignmentVisitor : SourceBaseVisitor<bool>
+{
+    private readonly SourceVisitor source;
+    private readonly ExpressionVisitor expressionVisitor;
+
+    public AssignmentVisitor(SourceVisitor source, ExpressionVisitor expressionVisitor)
+    {
+        this.source = source;
+        this.expressionVisitor = expressionVisitor;
+    }
+
+    public override bool VisitAssignment([NotNull] SourceParser.AssignmentContext context)
+    {
+        var id = context.ID().GetText();
+        var value = this.expressionVisitor.Visit(context.expression());
+        if (!this.source.Constants.TryAdd(id, value))
+        {
+            throw new ParsingException($"Failed to create constant \"{id}\" with value \"{value}\".", context);
+        }
+
+        return true;
+    }
+}
