c7f9b4cb089dbc3867489dc1f0c517ad8e404187
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-22 18:48:18 -0500

    Added music parser.

---
+++ PersonaMusicScript.Library/MusicParser.cs
@@ -0,0 +1,38 @@
+﻿using Antlr4.Runtime;
+using PersonaMusicScript.Library.Compiler;
+using PersonaMusicScript.Library.Parser;
+
+namespace PersonaMusicScript.Library;
+
+public class MusicParser
+{
+    private readonly MusicResources resources;
+
+    public MusicParser(string game)
+    {
+        this.resources = new(game);
+    }
+
+    public Music Parse(string inputFile, string? outputDir = null)
+    {
+        var inputStream = new AntlrInputStream(File.ReadAllText(inputFile));
+        var lexer = new SourceLexer(inputStream);
+        var tokens = new CommonTokenStream(lexer);
+        var sourceParser = new SourceParser(tokens);
+
+        var visitor = new SourceVisitor(this.resources);
+        var sourceCtx = sourceParser.source();
+        var musicSource = visitor.Visit(sourceCtx);
+        var music = new Music(musicSource, this.resources);
+
+        var outputFolder = outputDir ?? Path.Join(AppDomain.CurrentDomain.BaseDirectory, "output");
+        Directory.CreateDirectory(outputFolder);
+
+        var musicCompiler = new MusicCompiler(this.resources, musicSource);
+        musicCompiler.Compile(music, outputFolder);
+
+        var outputPresetFile = Path.Join(outputFolder, "BGME.project");
+        MusicPreset.Create(music, outputPresetFile);
+        return music;
+    }
+}
