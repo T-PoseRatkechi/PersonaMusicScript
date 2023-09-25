a6b1da8f632ff58c9e942088895f4f0be4becdac
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-25 18:43:25 -0500

    Added event types.

---
+++ PersonaMusicScript.Library/Models/EventFrame.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonaMusicScript.Library.Models;
+
+public class EventFrame
+{
+    public EventFrame(string eventFile)
+    {
+        this.EventFile = eventFile;
+    }
+
+    public string EventFile { get; }
+
+    public List<EventFrameBgm> FrameBgms { get; } = new();
+}
---
+++ PersonaMusicScript.Library/Models/EventFrameBgm.cs
@@ -0,0 +1,8 @@
+﻿namespace PersonaMusicScript.Library.Models;
+
+public class EventFrameBgm
+{
+    public ushort StartFrame { get; set; }
+
+    public ushort Bgm { get; set; }
+}
