20267d40108d02c155973c48c6ff81ebe372a084
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-27 13:27:00 -0500

    Added tv floors bgm compiler.

---
+++ PersonaMusicScript.Library/Compiler/TvFloorsCompiler.cs
@@ -0,0 +1,22 @@
+﻿namespace PersonaMusicScript.Library.Compiler;
+
+public class TvFloorsCompiler : IMusicCompiler
+{
+    public void Compile(Music music, List<string> patch, string outputDir)
+    {
+        var tvMusic = new List<ushort>(music.Resources.TvFloorsMusic);
+        foreach (var floor in music.Floors)
+        {
+            tvMusic[floor.Id] = floor.Music;
+        }
+
+        var tvBgmsIndex = patch.FindIndex(x => x.StartsWith("var tvBgms"));
+        if (tvBgmsIndex == -1)
+        {
+            throw new Exception("Failed to find var tvBgms");
+        }
+
+        var tvBgmBytes = Convert.ToHexString(tvMusic.SelectMany(BitConverter.GetBytes).ToArray());
+        patch[tvBgmsIndex] = $"var tvBgms({tvBgmBytes.Length}) = bytes({tvBgmBytes})";
+    }
+}
