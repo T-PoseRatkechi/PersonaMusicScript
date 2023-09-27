ed93f84ff9e0af95108393f303e2b35b8b0871ba
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-27 15:47:29 -0500

    Adjusted music.

--- PersonaMusicScript.Library/Music.cs
+++ PersonaMusicScript.Library/Music.cs
@@ -1,19 +1,24 @@
-﻿using PersonaMusicScript.Library.Models;
+﻿using PersonaMusicScript.Library.CommandBlocks;
+using PersonaMusicScript.Library.Models;
 using PersonaMusicScript.Library.Parser.Models;
 
 namespace PersonaMusicScript.Library;
 
 public class Music
 {
-    public Music(string game, MusicSource source, MusicResources resources)
+    private readonly ICommandBlock[] commandBlocks = new ICommandBlock[]
     {
-        this.Game = game;
+        new EncounterBlock(),
+        new EventBlock(),
+        new TvFloorBlock(),
+    };
+
+    public Music(MusicSource source, MusicResources resources)
+    {
         this.Source = source;
         this.Resources = resources;
         this.ProcessMusic();
     }
-
-    public string Game { get;}
 
     public MusicSource Source { get; }
 
@@ -23,142 +28,20 @@ public class Music
 
     public Dictionary<string, EventFrame> Events { get; } = new();
 
+    public HashSet<TvFloor> Floors { get; } = new();
+
     private void ProcessMusic()
     {
+        // Set all battle victory BGMs to default.
+        var victoryBgmCommand = new Command[]
+        {
+            new Command("victory_music", this.Resources.Constants.DefaultVictoryMusic)
+        };
+
+        this.Source.Blocks.Insert(0, new(CommandBlockType.Encounter, "all", victoryBgmCommand));
         foreach (var block in this.Source.Blocks)
         {
-            if (block.Type == CommandBlockType.Encounter)
-            {
-                if (block.Arg is int encounterId)
-                {
-                    var encounter = this.CreateEncounter(encounterId, block.Commands);
-                    this.Encounters.Add(encounter);
-                }
-                else if (block.Arg is string collectionName)
-                {
-                    if (this.Resources.Collections.TryGetValue(collectionName, out var ids))
-                    {
-                        foreach (var id in ids)
-                        {
-                            var encounter = this.CreateEncounter(id, block.Commands);
-                            this.Encounters.Add(encounter);
-                        }
-                    }
-                    else if (collectionName.ToLower() == "all")
-                    {
-                        for (int i = 0; i < this.Resources.Constants.TotalEncounters; i++)
-                        {
-                            var encounter = this.CreateEncounter(i, block.Commands);
-                            this.Encounters.Add(encounter);
-                        }
-                    }
-                }
-                else
-                {
-                    throw new Exception($"Invalid encounter block arg \"{block.Arg}\".");
-                }
-            }
-            else if (block.Type == CommandBlockType.Event)
-            {
-                var eventFile = block.Arg as string ?? throw new ArgumentException($"Invalid event block arg \"{block.Arg}\".");
-                foreach (var command in block.Commands)
-                {
-                    var frame = ushort.Parse(command.Name.Split('_')[1]);
-                    var musicId = GetMusicId(command.Value);
-
-                    if (this.Events.TryGetValue(eventFile, out var eventFrame))
-                    {
-                        eventFrame.FrameBgms.Add(new() { StartFrame = frame, Bgm = (ushort)musicId });
-                    }
-                    else
-                    {
-                        var newEvent = new EventFrame(eventFile);
-                        newEvent.FrameBgms.Add(new() { StartFrame = frame, Bgm = (ushort)musicId });
-                        this.Events.Add(eventFile, newEvent);
-                    }
-                }
-            }
-        }
-    }
-
-    private EncounterEntry CreateEncounter(int encounterId, IEnumerable<Command> commands)
-    {
-        var encounter = new EncounterEntry(encounterId);
-        IMusic? normalBgm = null;
-        IMusic? advantageBgm = null;
-        IMusic? disadvantageBgm = null;
-
-        foreach (var command in commands)
-        {
-            var musicType = this.GetMusicType(command.Value);
-            var musicId = this.GetMusicId(command.Value);
-            if (command.Name == "music")
-            {
-                encounter.Field04_1 = musicType;
-                encounter.Music = (ushort)musicId;
-            }
-            else if (command.Name == "victory_music")
-            {
-                encounter.Field04_2 = musicType;
-                encounter.Field06 = (ushort)musicId;
-            }
-            else if (command.Name == "normal_bgm")
-            {
-                normalBgm = command.Value as IMusic;
-            }
-            else if (command.Name == "advantage_bgm")
-            {
-                advantageBgm = command.Value as IMusic;
-            }
-            else if (command.Name == "disadvantage_bgm")
-            {
-                disadvantageBgm = command.Value as IMusic;
-            }
-            else
-            {
-                throw new Exception($"Unknown command \"{command.Name}\".");
-            }
-        }
-
-        if (normalBgm != null
-            || advantageBgm != null
-            || disadvantageBgm != null)
-        {
-            var battleBgm = new BattleBgm(this.Resources, this.Source, normalBgm, advantageBgm, disadvantageBgm);
-            encounter.Field04_1 = MusicType.BattleBgm;
-            encounter.Music = (ushort)battleBgm.Id;
-        }
-
-        return encounter;
-    }
-
-    private MusicType GetMusicType(object value)
-    {
-        if (value is int)
-        {
-            return MusicType.Song;
-        }
-        else if (value is IMusic music)
-        {
-            return music.Type;
-        }
-
-        throw new Exception("Invalid music type.");
-    }
-
-    private int GetMusicId(object value)
-    {
-        if (value is int songId)
-        {
-            return songId;
-        }
-        else if (value is IMusic music)
-        {
-            return music.Id;
-        }
-        else
-        {
-            throw new ArgumentException("Invalid music value.");
+            this.commandBlocks.First(x => x.Type == block.Type).Process(this, block);
         }
     }
 }
