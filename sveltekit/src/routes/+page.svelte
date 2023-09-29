d52c8a54aba27e696a5e7628429b7f9e0ef84184
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-28 21:42:05 -0500

    Fix link.

--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
 <h1>Welcome to Persona Music Script</h1>
-<p>Visit <a href="/beginner">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="{base}/beginner">kit.svelte.dev</a> to read the documentation</p>
