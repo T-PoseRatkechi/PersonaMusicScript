6867700747bd453b8c4111214276ad1124106475
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-09-28 22:38:12 -0500

    Added milligram css.

--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -1,6 +1,27 @@
 <script lang="ts">
+	import 'milligram';
 	import { base } from '$app/paths';
 </script>
 
-<h1>Welcome to Persona Music Script</h1>
-<p>Visit <a href="{base}/beginner">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+	<div class="row">
+		<div class="column">
+			<h1>Persona Music Script</h1>
+			<h2>Introduction</h2>
+			<p>
+				<em>Persona Music Script</em> is a tool for quickly and easily customizing music supported
+				Persona games. Write the changes you want to make in a text file, run
+				<em>Persona Music Script</em>, and then simply copy the generated files into a mod.
+			</p>
+			<h2>Quickstart</h2>
+			<p>TODO</p>
+			<h2>Beginner's Guide</h2>
+			<p>
+				This guide will walk you through creating your first custom music mod for Persona 4 Golden
+				PC. The changes you'll be making are: add randomized BGM to battles and giving a boss battle
+				its own song.
+			</p>
+			<a href="{base}/beginners-guide">Start the Beginner's Guide here.</a>
+		</div>
+	</div>
+</div>
