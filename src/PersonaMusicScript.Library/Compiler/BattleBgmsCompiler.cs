c7daf3449174964b6ca2d22e3ed34e1c7f56f930
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-10-02 23:54:36 -0500

    Added compilers for random songs and battle BGMs.

---
+++ src/PersonaMusicScript.Library/Compiler/BattleBgmsCompiler.cs
@@ -0,0 +1,51 @@
+﻿namespace PersonaMusicScript.Library.Compiler;
+
+public class BattleBgmsCompiler : IMusicCompiler
+{
+    public void Compile(Music music, List<string> patch, string outputDir)
+    {
+        // Create battle music data var.
+        var battleBgmsBytes = new List<byte>();
+        foreach (var battleBgm in music.Source.BattleBgms)
+        {
+            ushort config = 0;
+            var normalBytes = new byte[] { 0x00, 0x00 };
+            if (battleBgm.NormalBGM != null)
+            {
+                config = (byte)(config | (byte)battleBgm.NormalBGM.Type);
+                normalBytes = BitConverter.GetBytes((ushort)battleBgm.NormalBGM.Id);
+            }
+
+            config = (byte)(config << 2);
+
+            var advantageBytes = new byte[] { 0x00, 0x00 }; ;
+            if (battleBgm.AdvantageBGM != null)
+            {
+                config = (byte)(config | (byte)battleBgm.AdvantageBGM.Type);
+                advantageBytes = BitConverter.GetBytes((ushort)battleBgm.AdvantageBGM.Id);
+            }
+
+            config = (byte)(config << 2);
+
+            var disadvantageBytes = new byte[] { 0x00, 0x00 }; ;
+            if (battleBgm.DisadvantageBGM != null)
+            {
+                config = (byte)(config | (byte)battleBgm.DisadvantageBGM.Type);
+                disadvantageBytes = BitConverter.GetBytes((ushort)battleBgm.DisadvantageBGM.Id);
+            }
+
+            battleBgmsBytes.AddRange(BitConverter.GetBytes(config));
+            battleBgmsBytes.AddRange(normalBytes);
+            battleBgmsBytes.AddRange(advantageBytes);
+            battleBgmsBytes.AddRange(disadvantageBytes);
+        }
+
+        if (battleBgmsBytes.Count == 0)
+        {
+            battleBgmsBytes.Add(0x00);
+        }
+
+        var battleBgmsVar = $"var battleBgms({battleBgmsBytes.Count}) = bytes({Convert.ToHexString(battleBgmsBytes.ToArray())})";
+        patch.Insert(0, battleBgmsVar);
+    }
+}
---
+++ src/PersonaMusicScript.Library/Compiler/RandomSongsCompiler.cs
@@ -0,0 +1,25 @@
+﻿namespace PersonaMusicScript.Library.Compiler;
+
+public class RandomSongsCompiler : IMusicCompiler
+{
+    public void Compile(Music music, List<string> patch, string outputDir)
+    {
+        // Create random songs data var.
+        var randomSongsBytes = new List<byte>();
+        foreach (var randomSong in music.Source.RandomSongs)
+        {
+            var minBytes = BitConverter.GetBytes((ushort)randomSong.MinSongId);
+            var maxBytes = BitConverter.GetBytes((ushort)randomSong.MaxSongId);
+            randomSongsBytes.AddRange(minBytes);
+            randomSongsBytes.AddRange(maxBytes);
+        }
+
+        if (randomSongsBytes.Count == 0)
+        {
+            randomSongsBytes.Add(0x00);
+        }
+
+        var randomSongsVar = $"var randomSongs({randomSongsBytes.Count}) = bytes({Convert.ToHexString(randomSongsBytes.ToArray())})";
+        patch.Insert(0, randomSongsVar);
+    }
+}
