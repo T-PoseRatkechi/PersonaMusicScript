fd53cd9b7a89900faf813a03a365efe261d8313c
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-10-23 21:57:56 -0500

    Update music parser.

--- src/PersonaMusicScript.Library/MusicParser.cs
+++ src/PersonaMusicScript.Library/MusicParser.cs
@@ -1,5 +1,4 @@
 ﻿using Antlr4.Runtime;
-using PersonaMusicScript.Library.Compiler;
 using PersonaMusicScript.Library.Parser;
 
 namespace PersonaMusicScript.Library;
@@ -8,43 +7,27 @@ public class MusicParser
 {
     private readonly MusicResources resources;
 
-    public MusicParser(string game)
+    public MusicParser(string game, string? resourcesDir = null)
     {
-        this.resources = new(game);
+        this.resources = new(game, resourcesDir);
     }
 
-    public Music Parse(string inputFile)
+    /// <summary>
+    /// Parse music script from text file.
+    /// </summary>
+    /// <param name="inputFile">Input file path.</param>
+    /// <param name="existingSource">Existing music source to apply script to.</param>
+    /// <returns>Music source.</returns>
+    public MusicSource ParseFile(string inputFile, MusicSource? existingSource = null)
     {
         var inputStream = new AntlrInputStream(File.ReadAllText(inputFile));
         var lexer = new SourceLexer(inputStream);
         var tokens = new CommonTokenStream(lexer);
-        var sourceParser = new SourceParser(tokens);
+        var parser = new SourceParser(tokens);
+        var visitor = new SourceVisitor(this.resources, existingSource);
 
-        var visitor = new SourceVisitor(this.resources);
-        var sourceCtx = sourceParser.source();
-        var musicSource = visitor.Visit(sourceCtx);
-        var music = new Music(musicSource, this.resources);
-
-        var fileName = Path.GetFileNameWithoutExtension(inputFile) ?? "BGME";
-        var inputFolder = Path.GetDirectoryName(Path.GetFullPath(inputFile)) ?? AppDomain.CurrentDomain.BaseDirectory;
-        var outputFolder = Path.Join(inputFolder, fileName);
-        if (outputFolder.Length < "BGME".Length)
-        {
-            throw new Exception($"Output folder path shorter than expected. Path: {outputFolder}");
-        }
-
-        if (Directory.Exists(outputFolder))
-        {
-            Directory.Delete(outputFolder, true);
-        }
-
-        Directory.CreateDirectory(outputFolder);
-
-        var musicCompiler = new MusicCompiler();
-        musicCompiler.Compile(music, outputFolder);
-
-        var outputPresetFile = Path.Join(outputFolder, $"{fileName}.project");
-        MusicPreset.Create(music, outputPresetFile);
-        return music;
+        var sourceCtx = parser.source();
+        var source = visitor.Visit(sourceCtx);
+        return source;
     }
 }
