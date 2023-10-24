71bff59f30571a9ec03411c3fd160aca9b8f796a
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-10-23 21:39:45 -0500

    Add function table type.

---
+++ src/PersonaMusicScript.Library/Parser/Models/FunctionTable.cs
@@ -0,0 +1,11 @@
+﻿using PersonaMusicScript.Library.Parser.Functions;
+
+namespace PersonaMusicScript.Library.Parser.Models;
+
+internal class FunctionTable : Dictionary<string, Func<SourceParser.FunctionContext, object?>>
+{
+    public void Add<T>(IFunction<T> function)
+    {
+        this.Add(function.Name, (context) => function.Invoke(context));
+    }
+}
