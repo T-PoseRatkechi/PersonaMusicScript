4ea8ca8f3e649eb7de43acc09b5cddf2a7927613
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-10-26 21:04:10 -0500

    Add link component.

---
+++ sveltekit/src/lib/Link.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let text: string | null = null;
+	export let url: string;
+</script>
+
+{#if text}
+	<a href={url}>{text}</a>
+{:else}
+	<a href={url}><slot /></a>
+{/if}
