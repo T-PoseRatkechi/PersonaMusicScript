fb8b1199fe4b88a937847734c8dcc71cb755278a
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-10-28 06:08:57 -0500

    Add sound type.

--- src/PersonaMusicScript.Library/Models/MusicType.cs
+++ src/PersonaMusicScript.Library/Models/MusicType.cs
@@ -6,4 +6,5 @@ public enum MusicType : byte
     Song,
     RandomSong,
     BattleBgm,
+    Sound,
 }
---
+++ src/PersonaMusicScript.Library/Models/Sound.cs
@@ -0,0 +1,19 @@
+﻿namespace PersonaMusicScript.Library.Models;
+
+public class Sound : IMusic
+{
+    public Sound(IMusic music)
+    {
+        this.Music = music;
+    }
+
+    public IMusic Music { get; }
+
+    public int Setting_1 { get; set; }
+
+    public int Setting_2 { get; set; }
+
+    public int Setting_3 { get; set; }
+
+    public MusicType Type { get; } = MusicType.Sound;
+}
