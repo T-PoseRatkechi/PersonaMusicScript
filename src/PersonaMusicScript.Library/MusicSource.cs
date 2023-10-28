26a759fbed16899a23f88ce5c962ed6b0a1da8f7
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-10-28 06:13:00 -0500

    Use event IDs for events key.

--- src/PersonaMusicScript.Library/MusicSource.cs
+++ src/PersonaMusicScript.Library/MusicSource.cs
@@ -8,7 +8,32 @@ public class MusicSource
 
     public Dictionary<int, IMusic> Floors { get; } = new();
 
-    public Dictionary<string, EventFrame> Events { get; } = new();
+    public Dictionary<EventIds, EventFrame> Events { get; } = new();
 
     public Dictionary<int, IMusic> Global { get; } = new();
+
+    public void AddEventFrame(EventFrame frame)
+    {
+        this.Events.Add(frame.Ids, frame);
+    }
+
+    public EventFrame? GetEventFrame(int majorId, int minorId)
+    {
+        if (this.Events.TryGetValue(new(majorId, minorId), out var frame))
+        {
+            return frame;
+        }
+
+        return null;
+    }
+
+    public EventFrame? GetEventFrame(EventIds ids)
+    {
+        if (this.Events.TryGetValue(ids, out var frame))
+        {
+            return frame;
+        }
+
+        return null;
+    }
 }
