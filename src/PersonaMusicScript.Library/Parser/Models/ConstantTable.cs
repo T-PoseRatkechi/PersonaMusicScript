97b7bc11dd8cf1fe39f4a9d6c62c84cc0d53d8e1
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-10-31 16:47:45 -0500

    Add PMD bgm types as constants.

--- src/PersonaMusicScript.Library/Parser/Models/ConstantTable.cs
+++ src/PersonaMusicScript.Library/Parser/Models/ConstantTable.cs
@@ -1,5 +1,14 @@
-﻿namespace PersonaMusicScript.Library.Parser.Models;
+﻿using LibellusLibrary.Event.Types.Frame;
+
+namespace PersonaMusicScript.Library.Parser.Models;
 
 internal class ConstantTable : Dictionary<string, object>
 {
+    public ConstantTable()
+    {
+        foreach (var bgmType in Enum.GetValues<PmdBgmType>())
+        {
+            this.Add(bgmType.ToString(), bgmType);
+        }
+    }
 }
