62aff9abe16b1890cc43d523cb6e89c7e6d05e19
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-03 06:28:58 -0500

    Link checks if a slot is present, if not uses url as text.

--- sveltekit/src/lib/Link.svelte
+++ sveltekit/src/lib/Link.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	export let text: string | null = null;
 	export let url: string;
+
+	const hasSlot = $$slots.default;
 </script>
 
 {#if text}
 	<a href={url}>{text}</a>
-{:else}
+{:else if hasSlot}
 	<a href={url}><slot /></a>
+{:else}
+	<a href={url}>{url}</a>
 {/if}
