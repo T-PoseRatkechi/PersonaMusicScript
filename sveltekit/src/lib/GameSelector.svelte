845076a09b4c1f2a3accaf114c37a52deb5f9eea
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-04 22:11:03 -0500

    Add game selector component.

---
+++ sveltekit/src/lib/GameSelector.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let value: string;
+	import { allGames } from '$lib/Games';
+
+	value = allGames[0];
+</script>
+
+<select bind:value>
+	{#each allGames as game}
+		<option>{game}</option>
+	{/each}
+</select>
+
+<style>
+	select {
+		margin: 0;
+	}
+</style>
