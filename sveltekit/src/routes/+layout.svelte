2ec79ad6504e0c63d6384a6878e40930f2c7e039
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-04 22:14:55 -0500

    Added game selector to nav.

--- sveltekit/src/routes/+layout.svelte
+++ sveltekit/src/routes/+layout.svelte
@@ -2,14 +2,23 @@
 	import 'milligram';
 	import { base } from '$app/paths';
 	import Link from '$lib/Link.svelte';
+	import { currentGame } from '$lib/Games';
+	import GameSelector from '$lib/GameSelector.svelte';
 </script>
 
 <div id="nav">
 	<div class="container">
-		<Link url="{base}/" text="Home" />
-		<Link url="{base}/guides" text="Guides" />
-		<Link url="{base}/docs" text="Documentation" />
-		<Link url="{base}/docs/collections" text="Collections" />
+		<div class="row">
+			<div id="links" class="column">
+				<Link url="{base}/" text="Home" />
+				<Link url="{base}/guides" text="Guides" />
+				<Link url="{base}/docs" text="Documentation" />
+				<Link url="{base}/docs/collections" text="Collections" />
+			</div>
+			<div class="column">
+				<GameSelector bind:value={$currentGame} />
+			</div>
+		</div>
 	</div>
 </div>
 
@@ -33,5 +42,14 @@
 
 	:global(#nav a) {
 		padding: 0 1rem;
+	}
+
+	#links {
+		display: flex;
+		align-items: center;
+	}
+
+	:global(blockquote.warning) {
+		border-left: solid 0.3rem rgb(255, 228, 17);
 	}
 </style>
