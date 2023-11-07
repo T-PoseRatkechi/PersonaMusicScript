3adf4f57df12f466acbe194d173271ec21af596f
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-07 10:38:10 -0600

    Add project preset builder.

---
+++ src/PersonaMusicScript.Library/Presets/PresetBuilder.cs
@@ -0,0 +1,36 @@
+﻿using Phos.MusicManager.Library.Projects;
+using Phos.MusicManager.Library.Serializers;
+
+namespace PersonaMusicScript.Library.Presets;
+
+internal class PresetBuilder
+{
+    private readonly MusicResources resources;
+
+    public PresetBuilder(MusicResources resources)
+    {
+        this.resources = resources;
+    }
+
+    public void Create(MusicSource source, string outputFile)
+    {
+        var musicUse = new MusicUse(source);
+        var tracks = musicUse.Select(x => new PresetAudioTrack()
+        {
+            Name = $"BGM ID: {x.Key}",
+            Category = "BGME Framework",
+            Tags = x.Value.ToArray(),
+            OutputPath = this.resources.Constants.GetOutputPath(x.Key),
+            Encoder = this.resources.Constants.Encoder,
+        }).ToArray();
+
+        var name = Path.GetFileNameWithoutExtension(outputFile);
+        var preset = new ProjectPreset()
+        {
+            Name = name,
+            DefaultTracks = tracks,
+        };
+
+        ProtobufSerializer.Serialize(outputFile, preset);
+    }
+}
