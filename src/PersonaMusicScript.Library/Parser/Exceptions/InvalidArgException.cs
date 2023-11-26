45428fe2345af18cbf6cf5474655af95230b27b2
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-25 19:06:47 -0600

    Add invalid arg exception.

---
+++ src/PersonaMusicScript.Library/Parser/Exceptions/InvalidArgException.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonaMusicScript.Library.Parser.Exceptions;
+
+internal class InvalidArgException : Exception
+{
+    public InvalidArgException(object arg)
+        : base($"Invalid command block argument type: {arg.GetType().Name}")
+    {
+    }
+}
