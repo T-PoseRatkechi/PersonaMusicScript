fd2912a3376d74f6e37d66a2cbb761024c6a18d6
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-02 18:18:19 -0600

    Add testing project.

---
+++ src/PersonaMusicScript.Tests/MusicParserTests.cs
@@ -0,0 +1,33 @@
+﻿using PersonaModdingMetadata.Shared.Games;
+using PersonaMusicScript.Library;
+
+namespace PersonaMusicScript.Tests;
+
+public class MusicParserTests
+{
+    [Fact]
+    void MusicParser_EncounterBlock_EncounterId()
+    {
+        var script =
+        "encounter[1]:\n" +
+        "music = 100\n" +
+        "end\n";
+
+        var parser = new MusicParser(Game.P4G_PC);
+        var exception = Record.Exception(() => parser.Parse(script));
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    void MusicParser_EncounterBlock_Collection()
+    {
+        var script =
+        "encounter[\"The Reaper\"]:\n" +
+        "music = 100\n" +
+        "end\n";
+
+        var parser = new MusicParser(Game.P4G_PC);
+        var exception = Record.Exception(() => parser.Parse(script));
+        Assert.Null(exception);
+    }
+}
---
+++ src/PersonaMusicScript.Tests/MusicResourcesTest.cs
@@ -0,0 +1,18 @@
+﻿using PersonaModdingMetadata.Shared.Games;
+using PersonaMusicScript.Types;
+
+namespace PersonaMusicScript.Tests;
+
+public class MusicResourcesTest
+{
+    [Fact]
+    void MusicResources_Collections_NormalBattlesExist()
+    {
+        foreach (var game in Enum.GetValues<Game>())
+        {
+            var resources = new MusicResources(game);
+            var normalBattles = resources.Collections["Normal Battles"];
+            Assert.True(normalBattles.Length > 0, $"{game}: Empty normal battles.");
+        }
+    }
+}
