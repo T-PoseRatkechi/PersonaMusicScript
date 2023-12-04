aabaaef0c2f950f542c8112339668b8fad369e10
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-04 02:22:51 -0600

    Update tests.

--- src/PersonaMusicScript.Tests/MusicParserTests.cs
+++ src/PersonaMusicScript.Tests/MusicParserTests.cs
@@ -6,7 +6,7 @@ namespace PersonaMusicScript.Tests;
 public class MusicParserTests
 {
     [Fact]
-    void MusicParser_EncounterBlock_EncounterId()
+    public void MusicParser_EncounterBlock_EncounterId()
     {
         var script =
         "encounter[1]:\n" +
@@ -19,7 +19,7 @@ public class MusicParserTests
     }
 
     [Fact]
-    void MusicParser_EncounterBlock_Collection()
+    public void MusicParser_EncounterBlock_Collection()
     {
         var script =
         "encounter[\"The Reaper\"]:\n" +
@@ -29,5 +29,51 @@ public class MusicParserTests
         var parser = new MusicParser(Game.P4G_PC);
         var exception = Record.Exception(() => parser.Parse(script));
         Assert.Null(exception);
+    }
+
+    [Theory]
+    [InlineData("const set = battle_victory_set(1)", false)]
+    [InlineData("const set = battle_victory_set(1, 2)", true)]
+    [InlineData("const set = battle_victory_set(1, 2, 3)", false)]
+    public void MusicParser_BattleVictorySet_Compiles(string script, bool compiles)
+    {
+        var parser = new MusicParser(Game.P4G_PC);
+        bool actual = false;
+
+        try
+        {
+            parser.Parse(script);
+            actual = true;
+            Assert.True(actual == compiles);
+
+        }
+        catch (Exception ex)
+        {
+            Assert.True(actual == compiles, ex.Message);
+        }
+    }
+
+    [Theory]
+    [InlineData("const randomMusic = random_music(1)", false)]
+    [InlineData(
+        "const array = [1, 2, random_song(1, 5)]" +
+        "const randomMusic = random_music(array)",
+        true)]
+    public void MusicParser_RandomMusic_Compiles(string script, bool compiles)
+    {
+        var parser = new MusicParser(Game.P4G_PC);
+        bool actual = false;
+
+        try
+        {
+            parser.Parse(script);
+            actual = true;
+            Assert.True(actual == compiles);
+
+        }
+        catch (Exception ex)
+        {
+            Assert.True(actual == compiles, ex.Message);
+        }
     }
 }
