47c06382a25064636d50799401049922dff5c4ba
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-04 02:21:18 -0600

    Add support for BattleVictorySet music to encounters.

--- src/PersonaMusicScript.Types/MusicCollections/Entries/EncounterEntry.cs
+++ src/PersonaMusicScript.Types/MusicCollections/Entries/EncounterEntry.cs
@@ -4,6 +4,11 @@ namespace PersonaMusicScript.Types.MusicCollections.Entries;
 
 public class EncounterEntry : BaseEntry
 {
+    private IMusic? battleMusic;
+    private IMusic? victoryMusic;
+
+    private IMusic? victoryFromSet;
+
     public EncounterEntry(int encounterId)
         : base(encounterId)
     {
@@ -16,7 +21,50 @@ public class EncounterEntry : BaseEntry
         Name = collectionName;
     }
 
-    public IMusic? BattleMusic { get; set; }
+    public IMusic? BattleMusic
+    {
+        get
+        {
+            var currentMusic = this.battleMusic;
 
-    public IMusic? VictoryMusic { get; set; }
+            // Pre select a random music to handle
+            // random battle victory sets.
+            if (currentMusic is RandomMusic randomMusic)
+            {
+                currentMusic = randomMusic.GetRandomMusic();
+            }
+
+            // Set battle+victory music to music from set.
+            if (currentMusic is BattleVictorySet battleVictorySet)
+            {
+                this.victoryFromSet = battleVictorySet.VictoryMusic;
+                return battleVictorySet.BattleMusic;
+            }
+            
+            // Clear any victory music from a previous set.
+            else
+            {
+                this.victoryFromSet = null;
+            }
+
+            return currentMusic;
+        }
+
+        set => this.battleMusic = value;
+    }
+
+    public IMusic? VictoryMusic
+    {
+        get
+        {
+            if (this.victoryFromSet != null)
+            {
+                return this.victoryFromSet;
+            }
+
+            return this.victoryMusic;
+        }
+
+        set => this.victoryMusic = value;
+    }
 }
